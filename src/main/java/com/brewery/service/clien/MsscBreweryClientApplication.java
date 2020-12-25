671e81e255ca877975b19fd0325366d08707ba14
brabii <collectorsgarbages@gmail.com>
2020-12-25 14:48:00 +0100

    créer brewery-service-client. closes #1

---
+++ src/main/java/com/brewery/service/clien/MsscBreweryClientApplication.java
@@ -0,0 +1,13 @@
+package com.brewery.service.clien;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsscBreweryClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsscBreweryClientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/brewery/service/clien/MsscBreweryClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brewery.service.clien;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsscBreweryClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
